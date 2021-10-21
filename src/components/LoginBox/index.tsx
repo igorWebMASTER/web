806895253a5c6df82663589b131847de4eff0696
igorWebMASTER <igor_1917@hotmail.com>
2021-10-21 00:02:18 -0300

    chore: add styles of page

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,15 @@
+import { VscGithubInverted } from 'react-icons/vsc';
+
+import styles from './styles.module.scss';
+
+export function LoginBox(){
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe a sua mensagem</strong>
+            <a href="#" className={styles.sigInWithGithub}>
+                <VscGithubInverted  size={24} />
+                Entrar com github
+            </a>
+        </div>  
+    )
+}
