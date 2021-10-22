1bc14f43236a9847267ad4f71cddb5c90c37ddba
igorWebMASTER <igor_1917@hotmail.com>
2021-10-21 23:44:14 -0300

    chore: add auth context

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,27 @@
+import { createContext, ReactNode } from "react";
+
+type User = {
+    id: string;
+    name: string;
+    login: string;
+    avatar_url: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    sigInUrl: string;
+}
+
+const AuthContext = createContext({} as AuthContextData) 
+
+type AuthProvider = {
+    children: ReactNode;
+}
+
+export function AuthProvider(props : AuthProvider){
+    return (
+        <AuthContext.Provider value={null}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
